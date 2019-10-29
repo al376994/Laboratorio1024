713bedc87fc819ca4908c79303d804c2661d4bb0
al376994 <al376994@uji.es>
2019-10-29 12:30:21 +0100

    Commit inicial

---
+++ src/main/java/LA03/EjemploCuentaIncrementos1a.java
@@ -0,0 +1,89 @@
+package LA03;
+
+// ============================================================================
+class CuentaIncrementos {
+// ============================================================================
+  int numIncrementos = 0;
+
+  // --------------------------------------------------------------------------
+  void incrementaNumIncrementos() {
+    numIncrementos++;
+  }
+
+  // --------------------------------------------------------------------------
+  int dameNumIncrementos() {
+    return( numIncrementos );
+  }
+}
+
+
+// ============================================================================
+class MiHebra extends Thread {
+// ============================================================================
+  int                  tope;
+  CuentaIncrementos  c;
+
+  // --------------------------------------------------------------------------
+  public MiHebra( int tope, CuentaIncrementos c ) {
+    this.tope  = tope;
+    this.c     = c;
+  }
+
+  // --------------------------------------------------------------------------
+  public void run() {
+    for( int i = 0; i < tope; i++ ) {
+      c.incrementaNumIncrementos();
+    }
+  }
+}
+
+// ============================================================================
+class EjemploCuentaIncrementos {
+// ============================================================================
+
+  // --------------------------------------------------------------------------
+  public static void main( String args[] ) {
+    long    t1, t2;
+    double  tt;
+    int     numHebras, tope;
+
+    // Comprobacion y extraccion de los argumentos de entrada.
+    if( args.length != 2 ) {
+      System.err.println( "Uso: java programa <numHebras> <tope>" );
+      System.exit( -1 );
+    }
+    try {
+      numHebras = Integer.parseInt( args[ 0 ] );
+      tope      = Integer.parseInt( args[ 1 ] );
+    } catch( NumberFormatException ex ) {
+      numHebras = -1;
+      tope      = -1;
+      System.out.println( "ERROR: Argumentos numericos incorrectos." );
+      System.exit( -1 );
+    }
+
+    System.out.println( "numHebras: " + numHebras );
+    System.out.println( "tope:      " + tope );
+    
+    MiHebra v[] = new MiHebra[ numHebras ];
+    CuentaIncrementos c = new CuentaIncrementos();
+    t1 = System.nanoTime();
+    System.out.println( "Creando y arrancando " + numHebras + " hebras." );
+    for( int i = 0; i < numHebras; i++ ) {
+      v[ i ] = new MiHebra( tope, c );
+      v[ i ].start();
+    }
+    for( int i = 0; i < numHebras; i++ ) {
+      try {
+        v[ i ].join();
+      } catch( InterruptedException ex ) {
+        ex.printStackTrace();
+      }
+    }
+    t2 = System.nanoTime();
+    tt = ( ( double ) ( t2 - t1 ) ) / 1.0e9;
+    System.out.println( "Total de incrementos: " + c.dameNumIncrementos() );
+    System.out.println( "Tiempo transcurrido en segs.: " + tt );
+  }
+}
+
---
+++ src/main/java/LA03/EjemploMuestraPrimosEnVector2a.java
@@ -0,0 +1,98 @@
+package LA03;
+
+// ===========================================================================
+public class EjemploMuestraPrimosEnVector2a {
+// ===========================================================================
+
+  // -------------------------------------------------------------------------
+  public static void main( String args[] ) {
+    int     numHebras;
+    long    t1, t2;
+    double  ts, tc, tb, td;
+    long    vectorNumeros[] = {
+                200000033L, 200000039L, 200000051L, 200000069L, 
+                200000081L, 200000083L, 200000089L, 200000093L, 
+                4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+                4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+                4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+                4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+                4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+                4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+                4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L
+            };
+    //// long    vectorNumeros[] = {
+                //// 200000033L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+                //// 200000039L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+                //// 200000051L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+                //// 200000069L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+                //// 200000081L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+                //// 200000083L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+                //// 200000089L, 4L, 4L, 4L, 4L, 4L, 4L, 4L,
+                //// 200000093L, 4L, 4L, 4L, 4L, 4L, 4L, 4L
+            //// };
+    // Comprobacion y extraccion de los argumentos de entrada.
+    if( args.length != 1 ) {
+      System.err.println( "Uso: java programa <numHebras>" );
+      System.exit( -1 );
+    }
+    try {
+      numHebras = Integer.parseInt( args[ 0 ] );
+    } catch( NumberFormatException ex ) {
+      numHebras = -1;
+      System.out.println( "ERROR: Argumentos numericos incorrectos." );
+      System.exit( -1 );
+    }
+    //
+    // Implementacion secuencial.
+    //
+    System.out.println( "" );
+    System.out.println( "Implementacion secuencial." );
+    t1 = System.nanoTime();
+    for( int i = 0; i < vectorNumeros.length; i++ ) {
+      if( esPrimo( vectorNumeros[ i ] ) ) {
+        System.out.println( "  Encontrado primo: " + vectorNumeros[ i ] );
+      }
+    }
+    t2 = System.nanoTime();
+    ts = ( ( double ) ( t2 - t1 ) ) / 1.0e9;
+    System.out.println( "Tiempo secuencial (seg.):                    " + ts );
+/*
+    //
+    // Implementacion paralela ciclica.
+    //
+    System.out.println( "" );
+    System.out.println( "Implementacion paralela ciclica." );
+    t1 = System.nanoTime();
+    // Gestion de hebras para la implementacion paralela ciclica
+    // ....
+    t2 = System.nanoTime();
+    tc = ( ( double ) ( t2 - t1 ) ) / 1.0e9;
+    System.out.println( "Tiempo paralela ciclica (seg.):              " + tc );
+    System.out.println( "Incremento paralela ciclica:                 " + ... );
+    //
+    // Implementacion paralela por bloques.
+    //
+    // ....
+    //
+    // Implementacion paralela dinamica.
+    //
+    // ....
+*/
+  }
+
+  // -------------------------------------------------------------------------
+  static boolean esPrimo( long num ) {
+    boolean primo;
+    if( num < 2 ) {
+      primo = false;
+    } else {
+      primo = true;
+      long i = 2;
+      while( ( i < num )&&( primo ) ) { 
+        primo = ( num % i != 0 );
+        i++;
+      }
+    }
+    return( primo );
+  }
+}
